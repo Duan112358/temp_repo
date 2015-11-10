1db86cf4ab88ac5b19d9d11cf9db6f9afdcd7bb8
钱锟 <qiankun@wepiao.com>
2015-11-10 18:39:21 +0800

    header style and header tip with style partly

--- src/app/pages/signup/index.jsx
+++ src/app/pages/signup/index.jsx
@@ -45,8 +45,13 @@ var Header = React.createClass({
 	render: function() {
 		return (
 			<header className='header'>
-				<img src={logo} />
-				<h1>“旅行者镜头”用户协议</h1>
+				<section className='top'>
+					<img src={logo} />
+					<h1>“旅行者镜头”用户协议</h1>
+				</section>
+				<section className='topinfo'>
+					重要提示：“旅行者镜头”是wesafari公司开发、运营、管理的一款应用软件产品，旨于为用户提供免费的软件使用许可及技术服务，实现信息内容的发布。wesafari公司可能根据自身对商业、适用法律与政策等要素的判断，随时更新本协议。
+				</section>
 			</header>
 		);
 	}
